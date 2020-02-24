a712a0a7ca5576f4fb36ad29d8cd58f5a5a4e74a
Lily Scott <me@suchipi.com>
2020-02-24 04:26:11 -0700

    Default loader: don't care about their babel config
    It makes things confusing for users if their having a babel config makes test-it able to do less. If they want to override the built-in babel config, they can use a custom loader.

--- packages/default-loader/index.js
+++ packages/default-loader/index.js
@@ -7,37 +7,24 @@ module.exports = (filename) => {
     return "module.exports = " + fs.readFileSync(filename);
   }
 
-  let config;
-  const maybeConfigPath = path.join(process.cwd(), "babel.config.js");
-  if (fs.existsSync(maybeConfigPath)) {
-    config = require(maybeConfigPath);
-  } else {
-    config = {
-      presets: [
-        ["@babel/preset-env", { targets: { node: "current" } }],
-        "@babel/preset-react",
-      ],
-      plugins: [
-        "@babel/plugin-proposal-class-properties",
-        "@babel/plugin-proposal-nullish-coalescing-operator",
-        "@babel/plugin-proposal-optional-chaining",
-      ],
-    };
-
-    if (filename.endsWith(".ts") || filename.endsWith(".tsx")) {
-      config.presets.push("@babel/preset-typescript");
-    } else {
-      config.plugins.push("@babel/plugin-transform-flow-strip-types");
-    }
-  }
-
-  // Always add the plugin that compiles ESM to commonjs,
-  // because we *have* to use commonjs in test-it, but people
-  // might be outputting ESM eg. for webpack to consume
-  config = {
-    ...config,
-    plugins: [...config.plugins, "@babel/plugin-transform-modules-commonjs"],
+  const config = {
+    presets: [
+      ["@babel/preset-env", { targets: { node: "current" } }],
+      "@babel/preset-react",
+    ],
+    plugins: [
+      "@babel/plugin-proposal-class-properties",
+      "@babel/plugin-proposal-nullish-coalescing-operator",
+      "@babel/plugin-proposal-optional-chaining",
+      "@babel/plugin-transform-modules-commonjs",
+    ],
   };
+
+  if (filename.endsWith(".ts") || filename.endsWith(".tsx")) {
+    config.presets.push("@babel/preset-typescript");
+  } else {
+    config.plugins.push("@babel/plugin-transform-flow-strip-types");
+  }
 
   const result = babel.transformFileSync(filename, config);
   return result.code;
