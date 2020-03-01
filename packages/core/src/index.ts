96399f2710a4b2fc23b4291763af25dc8314ab03
Lily Scott <me@suchipi.com>
2020-03-01 10:20:30 -0700

    Revert "Run tests concurrently again"
    This reverts commit b72ae1f64c14fe99a981cd3ada84c288ae6be522.

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,4 +1,3 @@
-import os from "os";
 import path from "path";
 import util from "util";
 import Bluebird from "bluebird";
@@ -274,7 +273,9 @@ export async function runTests(
       return results;
     },
     {
-      concurrency: os.cpus().length,
+      // TODO: the snapshot reporter messes stuff up when we run tests concurrently.
+      // We need to switch to a more robust system, like jest's snapshot state.
+      concurrency: 1,
     }
   );
 
