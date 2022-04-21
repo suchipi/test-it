93cac59a97484612fa82095fe51a7fed59b3b5f9
Lily Scott <git@suchipi.tv>
2022-04-21 17:08:11 -0600

    Bump all versions to 0.7.0

--- packages/tests/version/index.test.js
+++ packages/tests/version/index.test.js
@@ -5,8 +5,8 @@ test("version", async () => {
   const result = await runTestIt(["--version"]);
 
   expect(result.stdout).toMatchInlineSnapshot(`
-    "@test-it/cli: 0.6.7
-    @test-it/core: 0.6.6
+    "@test-it/cli: 0.7.0
+    @test-it/core: 0.7.0
     "
   `);
   expect(result.stderr).toBe("");
