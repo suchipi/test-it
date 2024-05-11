52ad010638c870c457169d7bfc9ec32272f505e5
Lily Skye <me@suchipi.com>
2024-05-10 18:50:13 -0600

    update "looks right" test

--- packages/tests/custom-url/index.test-it.js
+++ packages/tests/custom-url/index.test-it.js
@@ -1,3 +1,6 @@
 test("looks right", async () => {
+  document.addEventListener("DOMContentLoaded", () => {
+    document.body.style.fontFamily = "Helvetica";
+  });
   expect(await TestIt.captureScreenshot()).toMatchImageSnapshot();
 });
